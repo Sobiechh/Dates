eac2d4925e18b45fde5d3b276d26563a153b371f
Sobiech <ps@qisrisk.com>
2023-08-07 03:53:19 +0200

    downgrade to 3.7 httpx

--- api/dates/utils.py
+++ api/dates/utils.py
@@ -1,8 +1,7 @@
 import httpx
 
-
 async def fetch_fun_fact(month: int, day: int) -> str:
     url = f"http://numbersapi.com/{month}/{day}/date"
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
-        return response.text
+        return response.headers.get("content-type")
